ea2c7ab3640c6f08c0955a6878a486d72e77d6c8
wzslr321 <wiktor.zajac888@gmail.com>
2021-09-10 15:28:05 +0200

    valid two sum problem solution

--- leetcode/easy/two_sum/main.cpp
+++ leetcode/easy/two_sum/main.cpp
@@ -2,36 +2,54 @@
  * Of course I know that this solution is incredibly awful and it doesn't even
  * pass every test cases, but it is my first CP problem I've ever solved
  * so I keep it just to have an insight on how do I progress!
-*/
+ */
+#include <iostream>
+#include <map>
+#include <vector>
 
-class Solution {
-   public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, int> mv{};
-        map<int,int> mi{};
-        vector<int> ni{};
-        for (int i{0}; i < nums.size(); i++) {
-            int needed{target - nums[i]};
-            if (nums[i] == needed){
-                ni.push_back(i);
-            }
-            mv[nums[i]] = needed;
-            mi[i] = needed;
-            mi[needed] = i; 
-            if (nums[i] == mv[needed]) {
-                int st = mi[mv[needed]];
-                if(st == mi[needed]) {
-                    if(ni.size() <= 1) {
-                        continue;
-                    } else {
-                        return vector<int>{ni[0], ni[1]};
-                    }
-                }
-               const vector<int> values = {st, mi[needed]};
-                return values;
-            }
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    map<int, int> mv{};
+    map<int, int> mi1{};
+    map<int, int> mi2{};
+    vector<int> ni{};
+    int l{};
+    for (int i{0}; i < nums.size(); i++) {
+        l++;
+        int needed{target - nums[i]};
+        if (nums[i] == needed) {
+            ni.push_back(i);
         }
-        const vector<int> values = {};
-        return values;
+        mv[nums[i]] = needed;
+        mi1[i] = needed;
+        mi2[needed] = i;
+        if (nums[i] == mv[needed]) {
+            int st = mi2[mv[needed]];
+            if (st == mi2[needed]) {
+                if (ni.size() <= 1) {
+                    continue;
+                } else {
+                    return vector<int>{ni[0], ni[1]};
+                }
+            }
+            if (l < 2) continue;
+            const vector<int> values = {st, mi2[needed]};
+            return values;
+        }
     }
-};
+    const vector<int> values = {};
+    return values;
+}
+
+int main() {
+    vector<int> arr{-6, 1, 0, 4};
+    int target{-5};
+    auto values{twoSum(arr, target)};
+
+    for (int value : values) {
+        cout << value << '\n';
+    }
+
+    return 0;
+}
