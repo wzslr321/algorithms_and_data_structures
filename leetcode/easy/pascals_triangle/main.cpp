8919b835e762b543d081bbf7e4f82971f57ca9bf
wzslr321 <wiktor.zajac888@gmail.com>
2021-10-16 00:46:10 +0200

    pascals triangle

--- leetcode/easy/pascals_triangle/main.cpp
+++ leetcode/easy/pascals_triangle/main.cpp
@@ -8,39 +8,26 @@ using ll = long long;
 
 vector<vector<int>> generate(int numRows) {
     vector<vector<int>> v1{{1}};
-    vector<vector<int>> v{{1}, {1, 1}};  // 1 2 1
+    vector<vector<int>> v{{1}, {1, 1}};
     if (numRows == 1) return v1;
     if (numRows == 2) return v;
+    uint row{1};
     while (v.size() < numRows) {
         vector<int> nv{1};
         uint tmp{0};
-        for (auto i{1}; i < v.size(); i++) {
-            cout << tmp << '\n';
-            nv.push_back(v[i][tmp] + v[i][tmp + 1]);
-            cout << "Hello" << '\n';
+        for (auto i{1}; i < v[row].size(); i++) {
+            nv.push_back(v[row][tmp] + v[row][tmp + 1]);
             ++tmp;
         }
         nv.push_back(1);
         v.push_back(nv);
+        row++;
     }
-
-    /*
-    for (auto i{0}; i < v.size(); i++) {
-        for (auto j{0}; j < v[i].size(); j++) {
-            cout << v[i][j] << " ";
-        }
-        cout << '\n';
-    }
-    */
 
     return v;
 }
 
 int main() {
-    generate(3);
-    cout << "=========" << '\n';
-    generate(4);
-    cout << "=========" << '\n';
     generate(5);
 
     return 0;
