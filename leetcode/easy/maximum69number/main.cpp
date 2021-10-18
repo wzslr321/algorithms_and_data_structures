a5991cd13a011b464e9337c6d1d2281ac7580014
wzslr321 <wiktor.zajac888@gmail.com>
2021-10-18 18:22:07 +0200

    max69number

---
+++ leetcode/easy/maximum69number/main.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int maximum69Number(int num) {
+    string s = to_string(num);
+
+    for (auto i{0}; i < s.size(); ++i) {
+        if (s[i] == '6') {
+            s[i] = '9';
+            return stoi(s);
+        }
+    }
+    return num;
+}
+int main() {
+    cout << maximum69Number(9996) << '\n';
+
+    return 0;
+}
