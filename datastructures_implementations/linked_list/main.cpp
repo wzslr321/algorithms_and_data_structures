d25fddc5748efe397311be77b98487184cac9078
wzslr321 <wiktor.zajac888@gmail.com>
2021-10-24 22:37:37 +0200

    wtf is wrong

---
+++ datastructures_implementations/linked_list/main.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+
+struct node {
+    int data;
+    struct node* next;
+};
+
+class linked_list {
+   private:
+    node* head;
+    node* tail;
+
+   public:
+    linked_list() {
+        head = NULL;
+        tail = NULL;
+    }
+
+    void push_back(int val) {
+        node* tmp = new node;
+        tmp->data = val;
+        tmp->next = NULL;
+        if (head == NULL) {
+            head = tmp;
+            tail = tmp;
+        } else {
+            tail->next = tmp;
+            tail = tmp;
+        }
+    }
+
+    void insert(int val) {
+        node* tmp = new node;
+        tmp->data = val;
+        tmp->next = head;
+        head = tmp;
+        if (tail == NULL) {
+            tail = tmp;
+        }
+    }
+
+    void insert_at(int position, int val) {
+        if (position < 2) {
+            insert(val);
+            return;
+        }
+        node* previous = new node;
+        node* current = new node;
+        node* tmp = new node;
+        current = head;
+        for (auto i{1}; i < position; ++i) {
+            previous = current;
+            current = current->next;
+            if (current == NULL) {
+                push_back(val);
+                break;
+            };
+        }
+        tmp->data = val;
+        previous->next = tmp;
+        tmp->next = current;
+    }
+
+    void pop_front() {
+        /*
+         * It can be done only with / head = head->next \
+         * because of memory leaks, previous head would still
+         * be out there in the memory. We have to keep track of it
+         * in order to delete it after updating the head.
+         */
+
+        node* tmp = new node;
+        tmp = head;
+        head = head->next;
+        delete tmp;
+    }
+
+    void pop_back() {
+        node* previous = new node;
+        node* current = new node;
+        while (current->next != NULL) {
+            previous = current;
+            current = current->next;
+        }
+        previous->next = NULL;
+        tail = previous;
+    }
+
+    void display() {
+        node* temp = new node;
+        temp = head;
+        while (temp != NULL) {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+};
+
+int main() {
+    linked_list list;
+    list.insert(3);
+    list.push_back(5);
+    list.insert_at(-3, 2);
+    list.insert_at(7, 8);
+    list.insert_at(8, 9);
+    list.pop_front();
+    list.push_back(7);
+    list.push_back(5);
+    list.pop_back();
+    list.display();
+    return 0;
+}
