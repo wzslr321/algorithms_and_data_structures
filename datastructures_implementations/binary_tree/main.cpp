6735813bc18917745062469ce237bf75a6144c5d
wzslr321 <wiktor.zajac888@gmail.com>
2021-10-26 01:32:45 +0200

    ifs scare me, it could be worse though

--- datastructures_implementations/binary_tree/main.cpp
+++ datastructures_implementations/binary_tree/main.cpp
@@ -25,32 +25,62 @@ class binary_tree {
         if (root == NULL) {
             root = node;
         } else {
-            Node<T>* tmp = root;
-            auto found{false};
-            while (!found) {
+            auto tmp = root;
+            while (tmp->right != node && tmp->left != node) {
+                if (node->data == tmp->data) break;
                 if (node->data > tmp->data) {
-                    if (tmp->right == NULL) {
+                    if (tmp->right == NULL)
                         tmp->right = node;
-                        found = true;
-                        break;
-                    }
-                    tmp = tmp->right;
-                }
-                if (node->data < tmp->data) {
-                    if (tmp->left == NULL) {
+                    else
+                        tmp = tmp->right;
+                } else {
+                    if (tmp->left == NULL)
                         tmp->left = node;
-                        found = true;
-                        break;
-                    }
-                    tmp = tmp->left;
+                    else
+                        tmp = tmp->left;
                 }
             }
         }
     }
 
-    void display() {
-        cout << root->right->right->data;
+    void remove(T value) {
+        auto tmp = root;
+        auto previous = new Node<T>;
+        while (true) {
+            if (tmp->data == value) {
+                if (previous->right != NULL) {
+                    if (previous->right->data == value) {
+                        previous->right = NULL;
+                        delete tmp;
+                    }
+                }
+                if (previous->left != NULL) {
+                    if (previous->left->data == value) {
+                        previous->left = NULL;
+                        delete tmp;
+                    }
+                }
+                if (root->data == value) {
+                    root = tmp->right;
+                    root->left = tmp->left;
+                    delete tmp;
+                }
+                break;
+            }
+            if (tmp->data < value) {
+                if (tmp->right == NULL) break;
+                previous = tmp;
+                tmp = tmp->right;
+            }
+            if (tmp->data > value) {
+                if (tmp->left == NULL) break;
+                previous = tmp;
+                tmp = tmp->left;
+            }
+        }
     }
+
+    void display() { cout << root->right->right; }
 };
 
 auto main() -> int {
@@ -59,6 +89,7 @@ auto main() -> int {
     tree.insert(4);
     tree.insert(8);
     tree.insert(9);
+    tree.remove(9);
     tree.display();
 
     return 0;
