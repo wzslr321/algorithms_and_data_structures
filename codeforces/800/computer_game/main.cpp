260f534a71d208ed01ed5886b61dfb809c115e4e
wzslr321 <wiktor.zajac888@gmail.com>
2021-10-30 12:57:48 +0200

    cleaner computer game

--- codeforces/800/computer_game/main.cpp
+++ codeforces/800/computer_game/main.cpp
@@ -77,27 +77,16 @@ auto main() -> int {
     while (t--) {
         int n;
         cin >> n;
-        vector<vector<char>> lvl;
-        LPI(i, 0, 2, 1) {
-            vector<char> lv;
-            LPI(j, 0, n, 1) {
-                char ch;
-                cin >> ch;
-                lv.PB(ch);
-            }
-            lvl.PB(lv);
-        }
+        string s1, s2;
+        cin >> s1 >> s2;
         auto no{false};
         LPI(i, 0, n, 1) {
-            if (lvl[0][i] == '1' && lvl[1][i] == '1') {
+            if (s1[i] == '1' && s2[i] == '1') {
                 no = true;
                 break;
             }
         }
-        if (no)
-            cout << "NO\n";
-        else
-            cout << "YES\n";
+        no ? cout << "NO\n" : cout << "YES\n";
     }
 
     return 0;
