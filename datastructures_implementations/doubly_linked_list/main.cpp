f7b2fab698833c4a47f9d598e9f48e52887b864f
wzslr321 <wiktor.zajac888@gmail.com>
2021-10-31 20:24:47 +0100

    insert at

--- datastructures_implementations/doubly_linked_list/main.cpp
+++ datastructures_implementations/doubly_linked_list/main.cpp
@@ -36,12 +36,45 @@ struct doubly_linked_list {
         }
     }
 
+    void insert_at(int position, T value) {
+        if (position <= 1) {
+            return insert(value);
+        }
+        auto current = new Node<T>;
+        auto tmp = new Node<T>;
+        tmp->data = value;
+        for (auto i{1}; i < position; ++i) {
+            if (current->next == NULL) {
+                tmp->previous = current;
+                current->next = tmp;
+                tail = tmp;
+            } else {
+                current = current->next;
+            }
+        }
+        tmp->next = current->next;
+        current->next = tmp;
+        tmp->previous = current;
+    }
+
     void display() {
         auto tmp = new Node<T>;
+        auto tmp2 = new Node<T>;
         tmp = head;
+        tmp2 = tmp;
+        printf("LIST: ");
         while (tmp != NULL) {
             printf("%d ", tmp->data);
             tmp = tmp->next;
+        }
+        printf("\n");
+
+        while (tmp2 != NULL) {
+            printf("%d ", tmp2->data);
+            if (tmp2->previous) printf("previous: %d \t", tmp2->previous->data);
+            if (tmp2->next) printf("next: %d ", tmp2->next->data);
+            tmp2 = tmp2->next;
+            printf("\n");
         }
         printf("\n");
     }
@@ -51,6 +84,7 @@ auto main() -> int {
     doubly_linked_list<int> list;
     list.insert(5);
     list.insert(8);
+    list.insert(2);
     list.insert(9);
     list.display();
 
