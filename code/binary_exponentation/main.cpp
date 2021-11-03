65243f2ccd9ec57bf885453c85aaf01325fdab4c
wzslr321 <wiktor.zajac888@gmail.com>
2021-11-03 22:22:12 +0100

    binary exponentation

--- code/binary_exponentation/main.cpp
+++ code/binary_exponentation/main.cpp
@@ -17,15 +17,46 @@ auto binary_exponentation(ll a, ll b) -> ll {
     ll res = 1;
     while (b > 0) {
         if (b & 1) res = res * a;
-        a = a * a;
+        a *= a;
         b >>= 1;
     }
     return res;
 }
 
-auto main() -> int {
-    cout << binary_exponentation_recursive(9, 16) << '\n';
-    cout << binary_exponentation(9, 16) << '\n';
+auto normal_pow(ll a, ll b) -> ll {
+    ll res = 1;
+    while (b > 0) {
+        res *= a;
+        --b;
+    }
 
+    return res;
+}
+
+#define LL_MAX 9223372036854775807
+
+auto main() -> int {
+    clock_t begin = clock();
+    cout << binary_exponentation_recursive(LL_MAX, LL_MAX) << '\n';
+    clock_t end = clock();
+    cout << "\nExecuted in: " << double(end - begin) / CLOCKS_PER_SEC * 1000
+         << " ms\n\n";
+
+    cout << "--------------------\n";
+
+    begin = clock();
+    cout << binary_exponentation(LL_MAX, LL_MAX) << '\n';
+    end = clock();
+    cout << "\nExecuted in: " << double(end - begin) / CLOCKS_PER_SEC * 1000
+         << " ms\n\n";
+    
+    cout << "--------------------\n";
+
+    begin = clock();
+    // ints here, 'cause probably noone have enough time to test it on so large input
+    cout << normal_pow(INT_MAX, INT_MAX) << '\n';
+    end = clock();
+    cout << "\nExecuted in: " << double(end - begin) / CLOCKS_PER_SEC * 1000
+         << " ms\n";
     return 0;
 }
