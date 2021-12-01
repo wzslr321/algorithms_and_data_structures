048ff425c78713d3d07d6eaf59b75f3b4769cb97
wzslr321 <wiktor.zajac888@gmail.com>
2021-12-01 14:37:40 +0100

    rust version

---
+++ advent_of_code_2021/1/rust_version/target/debug/deps/rust_version-e5b3d1f35c950b58.d
@@ -0,0 +1,5 @@
+/Users/wiktor/Desktop/OI/advent_of_code_2021/1/rust_version/target/debug/deps/rust_version-e5b3d1f35c950b58.rmeta: src/main.rs
+
+/Users/wiktor/Desktop/OI/advent_of_code_2021/1/rust_version/target/debug/deps/rust_version-e5b3d1f35c950b58.d: src/main.rs
+
+src/main.rs:
---
+++ advent_of_code_2021/1/rust_version/target/debug/rust_version.d
@@ -0,0 +1,1 @@
+/Users/wiktor/Desktop/OI/advent_of_code_2021/1/rust_version/target/debug/rust_version: /Users/wiktor/Desktop/OI/advent_of_code_2021/1/rust_version/src/main.rs
