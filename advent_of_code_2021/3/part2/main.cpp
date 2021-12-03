fdb1acc3f6d1ab2b5436a64e4b4c1f81502d688d
wzslr321 <wiktor.zajac888@gmail.com>
2021-12-03 20:21:33 +0100

    day3 part2 finished

--- advent_of_code_2021/3/part2/main.cpp
+++ advent_of_code_2021/3/part2/main.cpp
@@ -1,9 +1,8 @@
 #include <cmath>
-#include <cstdlib>
 #include <iostream>
 #include <vector>
 
-void scan(const std::vector<std::string> &data, std::string &filter) {
+void scan(const std::vector<std::string> &data, std::string &filter, bool oxy) {
   for (int i{0}; i <= filter.size(); ++i) {
     int ones = 0;
     int zeros = 0;
@@ -23,13 +22,34 @@ void scan(const std::vector<std::string> &data, std::string &filter) {
       else
         ++zeros;
     }
-    if (matches.size() == 1) return;
-    if (ones >= zeros)
-      filter.push_back('1');
-    else
-      filter.push_back('0');
+    if (matches.size() == 1) {
+      filter = matches[0];
+      return;
+    }
+    if (!oxy) {
+      if (ones < zeros)
+        filter.push_back('1');
+      else
+        filter.push_back('0');
+    } else {
+      if (ones >= zeros)
+        filter.push_back('1');
+      else
+        filter.push_back('0');
+    }
   }
 }
+
+int to_decimal(const std::string &str) {
+  int i = 0;
+  int num = 0;
+  for (size_t j{str.size()}; j > 0; --j) {
+    if (str[j - 1] == '1') num += std::pow(2, i);
+    ++i;
+  }
+  return num;
+}
+
 auto main() -> int {
   std::vector<std::string> data;
   while (std::cin) {
@@ -39,11 +59,17 @@ auto main() -> int {
     data.push_back(s);
   }
 
-  std::string filter = "";
+  std::string filter_oxy = "";
+  std::string filter_co2 = "";
 
-  scan(data, filter);
+  scan(data, filter_oxy, true);
+  scan(data, filter_co2, false);
 
-  std::cout << filter << '\n';
+  std::cout << "oxygen: " << to_decimal(filter_oxy) << '\n';
+  std::cout << "co2: " << to_decimal(filter_co2) << '\n';
+
+  std::cout << "answer: " << (to_decimal(filter_oxy) * to_decimal(filter_co2))
+            << '\n';
 
   return 0;
 }
