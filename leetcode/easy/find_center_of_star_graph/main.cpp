c19147e151135089a611ab595acd8572f6c9f3eb
wzslr321 <wiktor.zajac888@gmail.com>
2021-12-08 22:33:22 +0100

    find center of star graph

---
+++ leetcode/easy/find_center_of_star_graph/main.cpp
@@ -0,0 +1,14 @@
+class Solution {
+ public:
+  int findCenter(vector<vector<int>> &edges) {
+    int p1 = edges[0][0];
+    int p2 = edges[0][1];
+
+    for (size_t i = 1; i < edges.size(); ++i) {
+      for (size_t j = 0; j < edges[i].size(); ++j) {
+        if (edges[i][j] == p1 || edges[i][j] == p2) return edges[i][j];
+      }
+    }
+    return p1;
+  }
+};
