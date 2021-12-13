f949083320341a752350861621724d84130855f4
wzslr321 <wiktor.zajac888@gmail.com>
2021-12-13 20:41:51 +0100

    shortest path, not fully valid

--- codeforces/1900/dijkstra/main.cpp
+++ codeforces/1900/dijkstra/main.cpp
@@ -47,14 +47,12 @@ constexpr double eps = 1e-10;
 constexpr int N = 1e2 + 10;
 
 template <typename T> T gcd(T a, T b) {
-  if (!a || !b)
-    return a | b;
+  if (!a || !b) return a | b;
   unsigned shift = __builtin_ctz(a | b);
   a >>= __builtin_ctz(a);
   do {
     b >>= __builtin_ctz(b);
-    if (a > b)
-      swap(a, b);
+    if (a > b) swap(a, b);
     b -= a;
   } while (b);
   return a << shift;
@@ -63,41 +61,53 @@ template <typename T> T gcd(T a, T b) {
 template <typename T> T LCM(T a, T b) { return a / GCD(a, b) * b; }
 
 struct Edge {
-    int from;
-    int to;
-    int weight;
-    Edge() : from(0), to(0), weight(0) {}
-    Edge(int f, int t, int w) : from(f), to(t), weight(w) {}
+  int from;
+  int to;
+  int weight;
+  Edge() : from(0), to(0), weight(0) {}
+  Edge(int f, int t, int w) : from(f), to(t), weight(w) {}
 };
 
-int dijkstra(vector<Edge> adj[], int n, int s) {
+void dijkstra(vector<Edge> adj[], int n, int s) {
   bool visited[n + 1];
-  fill_n(visited,n+1, false);
+  fill_n(visited, n + 1, false);
   int dist[n + 1];
-  fill_n(dist, n+1, INT_MAX);
+  fill_n(dist, n + 1, INT_MAX);
   dist[1] = 0;
+  bool reached = false;
 
-  priority_queue<pair<int,int>> pq;
-  pq.push(make_pair(s,0));
-  while(pq.size() > 0) {
-      int i = pq.top().first;
-      int min = pq.top().second;
-      pq.pop();
-      for(const auto &edge : adj[i]){
-          if(visited[edge.to]) continue;
-          auto ndist = dist[i] + edge.weight;
-          if(ndist < dist[edge.to]) {
-            dist[edge.to] = ndist;
-            pq.push(make_pair(edge.to, ndist));
-          }
+  priority_queue<pair<int, int>> pq;
+  pq.push(make_pair(s, 0));
+  vector<pair<int, int>> path{make_pair(0, 1)};
+
+  while (pq.size() > 0) {
+    if(reached) break;
+    int i = pq.top().first;
+    int min = pq.top().second;
+    pq.pop();
+    for (const auto &edge : adj[i]) {
+      if (visited[edge.to]) continue;
+      if (edge.to == n) reached = true;
+      auto ndist = dist[i] + edge.weight;
+      if (ndist < dist[edge.to]) {
+        dist[edge.to] = ndist;
+        if (path.back().first == edge.from) {
+          path.back().second = edge.to;
+        } else {
+          path.push_back(make_pair(edge.from, edge.to));
+        }
+        pq.push(make_pair(edge.to, ndist));
       }
-  }
- 
-  LPI(i,1,n + 1,1) {
-      cout << dist[i] << ' ';
+    }
   }
 
-  return 0;
+  if (!reached) {
+    cout << "-1\n";
+    return;
+  }
+  LPI(i, 0, path.size(), 1) { cout << path[i].second << ' '; }
+  cout << '\n';
+  return;
 }
 
 auto main() -> int {
@@ -107,19 +117,18 @@ auto main() -> int {
   ios::sync_with_stdio(false);
   cin.tie(0);
 
-  int n,m;
+  int n, m;
   cin >> n >> m;
-  vector<Edge> adj[n+1];
+  vector<Edge> adj[n + 1];
 
-  LPI(i,0,m,1) {
-      int a,b,w;
-      cin >> a >> b >> w;
-      adj[a].push_back(Edge(a,b,w));
-      adj[b].push_back(Edge(b,a,w));
+  LPI(i, 0, m, 1) {
+    int a, b, w;
+    cin >> a >> b >> w;
+    adj[a].push_back(Edge(a, b, w));
+    adj[b].push_back(Edge(b, a, w));
   }
 
-  int length = dijkstra(adj, n, 1);
-  cout << length << '\n';
+  dijkstra(adj, n, 1);
 
   return 0;
 }
