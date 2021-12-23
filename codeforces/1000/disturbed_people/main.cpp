51eb70477516c2fd33754226e4f3dcaba7dc6d63
wzslr321 <wiktor.zajac888@gmail.com>
2021-12-23 17:28:11 +0100

    disturbed people

---
+++ codeforces/1000/disturbed_people/main.cpp
@@ -0,0 +1,43 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+#define PB push_back
+#define MP make_pair
+
+#define LPI(i, a, b, d) for (int i = a; i < b; i += d)
+
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VS = vector<string>;
+using VVS = vector<VS>;
+using PI = pair<int, int>;
+
+auto main() -> int {
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n;
+  cin >> n;
+  int arr[n];
+  LPI(i, 0, n, 1) { cin >> arr[i]; }
+  int ans = 0;
+  LPI(i, 0, n, 1) {
+    if (i > 0 && i < n - 1) {
+      if (arr[i - 1] == 1 && arr[i] == 0 && arr[i + 1] == 1) {
+        arr[i + 1] = 0;
+        ++ans;
+      }
+    }
+  }
+  cout << ans << '\n';
+
+  return 0;
+}
