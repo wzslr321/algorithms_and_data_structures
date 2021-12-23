4408569b73ffbab48c9461c6602241f1ae15707c
wzslr321 <wiktor.zajac888@gmail.com>
2021-12-23 19:24:46 +0100

    kings task failure

---
+++ codeforces/900/party/main.cpp
@@ -0,0 +1,33 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+#define PB push_back
+#define MP make_pair
+
+#define LPI(i, a, b, d) for (int i = a; i < b; i += d)
+
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VS = vector<string>;
+using VVS = vector<VS>;
+using PI = pair<int, int>;
+
+auto main() -> int {
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n;
+  cin >> n;
+  int arr[n];
+  LPI(i, 0, n, 1) { cin >> arr[i]; }
+
+  return 0;
+}
