8e0738f57cc1f7fcfabd81528eb6e95b004ba05d
wzslr321 <wiktor.zajac888@gmail.com>
2021-12-27 19:36:58 +0100

    surrender. 🥷

--- codeforces/2400/purple_crayon/main.cpp
+++ codeforces/2400/purple_crayon/main.cpp
@@ -23,17 +23,19 @@ ll n, k;
 const int N = 2 * 10e5 + 1;
 int max_deep = INT_MIN;
 
-ll r = 0LL;
+ll red = 0LL;
 ll blue = 0LL;
 
 bool visited_dfs[N];
 bool visited_c[N];
 bool visited_p[N];
 int infected[N];
+ll infected_count = 1LL;
 
 unordered_map<int, pair<int, vector<int>>> edges;
 unordered_map<int, int> parent;
 unordered_map<int, vector<int>> deepest;
+vector<int> tc;
 
 void find_parents() {
   queue<int> qq;
@@ -72,80 +74,39 @@ void dfs() {
   }
 }
 
-// check if current node is worth painting
-bool should_paint(int node) {
-  stack<int> st;
-  st.push(node);
-  while (st.size() > 0) {
-    auto curr = st.top();
-    if (infected[curr]) return false;
-    st.pop();
-    for (auto ch : edges[curr].second) {
-      // similar functtionality to visited;
-      if (parent[curr] == ch) continue;
-      st.push(ch);
-    }
-  }
-  // no infected nodes found in this subtree, should be painted
-  return true;
-}
-
-// maybe colored leaves are not optimal to color, though should paint returnet true?
-// this greedness should work, but what if subtree that should be painted is 
-// not the largest not infected subtree, and there won't be enough crayons to color that bigger
-// subtrees later? 
+// maybe colored leaves are not optimal to color, though should paint returnet
+// true? this greedness should work, but what if subtree that should be painted
+// is not the largest not infected subtree, and there won't be enough crayons to
+// color that bigger subtrees later?
 void color_red() {
   int i = 0;
   while (max_deep >= 0) {
-    for (size_t j = 0; j < deepest[max_deep].size(); ++j) {
-      // answer is already maximized, don't color more
-      if (i == k || i == n / 2) return;
-      // tree is already infected, check next node;
-      if (!should_paint(deepest[max_deep][j])) continue;
+    int sz = deepest[max_deep].size();
+    int j = 0;
+    bool l = true;
+    while (j < sz) {
+      int in = l ? j : sz - j - 1;
+      if (i == k || infected_count == n) return;
+      // tree is already infected, check next node
+      if (infected[deepest[max_deep][in]]) {
+        ++j;
+        continue;
+      }
 
-      int current = parent[deepest[max_deep][j]];
+      int current = parent[deepest[max_deep][in]];
       while (current != 1) {
         if (infected[current] == true) break;
+        ++infected_count;
         infected[current] = true;
         current = parent[current];
       }
-      ++r, ++i;
-      infected[deepest[max_deep][j]] = true;
+      ++red, ++i;
+      ++infected_count;
+      infected[deepest[max_deep][in]] = true;
+      l ? l = false : l = true;
+      ++j;
     }
     --max_deep;
-  }
-  // if shouldn't paint occurred too many times, and
-  // colored are less than K, set r to maximize the value 🤕
-  // how though?
-  // r * (n - r) <-- it have to be maximized
-  // it will be maximized if abs((n/2) -  r) is as closest to n/2
-  if (i < k) {
-    // should paint occured so many times, that whole tree is infected,
-    // it doesn't matter which nodes we will color now, since there can't be
-    // blue nodes we just maximize r --> set it closest to n/2 if k > n/2 or k
-    // if less,
-    if (k < n / 2)
-      r = k;
-    else
-      r = n / 2;
-  }
-}
-
-void color_blue() {
-  queue<int> qq;
-  qq.push(1);
-  while (qq.size() > 0) {
-    auto curr = qq.front();
-    visited_c[curr] = true;
-    // already maximized
-    if (blue == n / 2) break;
-    // subtree not infected, color to blue
-    if (!infected[curr]) ++blue;
-    qq.pop();
-    for (auto ch : edges[curr].second) {
-      if (visited_c[ch]) continue;
-      qq.push(ch);
-    }
   }
 }
 
@@ -168,9 +129,19 @@ auto main() -> int {
   find_parents();
   dfs();
   color_red();
-  color_blue();
 
-  cout << ((n - r - blue) * (r - blue)) << '\n';
+  blue = n - infected_count;
+  if (infected_count == n) {
+    if (red != infected_count - 1) {
+      if (red < k) {
+        if (k > n / 2)
+          red = n / 2;
+        else
+          red = k;
+      }
+    }
+  }
+  cout << ((n - red - blue) * (red - blue)) << '\n';
 
   return 0;
 }
