9459201c65f6a821e601481dc7c3a21a56dc6cc5
wzslr321 <wiktor.zajac888@gmail.com>
2021-12-29 15:26:09 +0100

    regular bracket sequence

---
+++ codeforces/1000/regular_bracket_sequence/main.cpp
@@ -0,0 +1,34 @@
+#pragma GCC optimize("O3,unroll-loops")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+#define PB push_back
+#define MP make_pair
+
+#define LPI(i, a, b, d) for (int i = a; i < (int)b; i += d)
+
+using VI = vector<int>;
+using VS = vector<string>;
+using PI = pair<int, int>;
+
+auto main() -> int {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    string s;
+    cin >> s;
+    if (!(s.size() & 1) && s[0] != ')' && s.back() != '(')
+      cout << "YES\n";
+    else
+      cout << "NO\n";
+  }
+
+  return 0;
+}
