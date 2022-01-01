6e0fd3d6b8bfd56fab25749bf99d50d0b3695045
wzslr321 <wiktor.zajac888@gmail.com>
2022-01-01 16:09:21 +0100

    filling diamonds

---
+++ codeforces/900/filling_diamonds/main.cpp
@@ -0,0 +1,34 @@
+#pragma GCC optimize("O3,unroll-loops")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+#define PB push_back
+#define MP make_pair
+
+#define LPI(i, a, b, d) for (int i = a; i < (int)b; i += d)
+
+using VI = vector<int>;
+using VS = vector<string>;
+using PI = pair<int, int>;
+
+auto solve() -> void {
+  int n;
+  cin >> n;
+  cout << n << '\n';
+}
+
+auto main() -> int {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int _ = 1;
+  cin >> _;
+  while (_--)
+    solve();
+
+  return 0;
+}
