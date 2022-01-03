ed9a00d916ba5c2d7ab6ac5ef7b86ff2b95f3da3
wzslr321 <wiktor.zajac888@gmail.com>
2022-01-03 23:29:02 +0100

    some updates, hello 2022 🥷

--- template.cpp
+++ template.cpp
@@ -9,20 +9,24 @@ using namespace std;
 #define PB push_back
 #define MP make_pair
 
-#define LPI(i, a, b, d) for (int i = a; i < b; i += d)
+#define LPI(i, a, b, d) for (int i = a; i < (int)b; i += d)
 
 using VI = vector<int>;
 using VS = vector<string>;
 using PI = pair<int, int>;
 
+auto solve() -> void {
+
+}
+
 auto main() -> int {
   ios::sync_with_stdio(false);
   cin.tie(0);
 
-  int t;
-  cin >> t;
-  while (t--) {
-  }
+  int _ = 1;
+  cin >> _;
+  while (_--)
+    solve();
 
   return 0;
 }
