7baf175ce0d47b1172b593c972b1704cc76c2a4f
wzslr321 <wiktor.zajac888@gmail.com>
2022-01-04 18:25:40 +0100

    zuhair and strings

---
+++ codeforces/1100/zuhair_and_strings/cmake-build-debug/CMakeFiles/zuhair_and_strings.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zuhair_and_strings.dir/main.cpp.o"
+  "zuhair_and_strings"
+  "zuhair_and_strings.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zuhair_and_strings.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
