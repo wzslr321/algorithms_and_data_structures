a66b606763d85bcea8e82d63f4dec03b31b64534
wzslr321 <wiktor.zajac888@gmail.com>
2022-01-15 20:10:07 +0100

    corner cases failing in not escaping problem E

--- codeforces/contests/div3_766/not_escaping/main.cpp
+++ codeforces/contests/div3_766/not_escaping/main.cpp
@@ -8,85 +8,92 @@ using namespace std;
 
 #define PB push_back
 
-#define LPI(i, a, b, d) for (int (i) = a; (i) < (int)(b); (i) += (d))
+#define LPI(i, a, b, d) for (int(i) = a; (i) < (int)(b); (i) += (d))
 
 using PI = pair<int, int>;
 
 // make it possible to store pair as map key
 namespace std {
-    template<typename T, typename U>
-    struct hash<std::pair<T, U>> {
-        auto operator()(const std::pair<T, U> &pair) const noexcept {
-            auto const hash_t = std::hash<T>{}(pair.first);
-            auto const hash_u = std::hash<U>{}(pair.second);
-            return hash_t ^ hash_u;
-        }
-    };
+template <typename T, typename U> struct hash<std::pair<T, U>> {
+  auto operator()(const std::pair<T, U> &pair) const noexcept {
+    auto const hash_t = std::hash<T>{}(pair.first);
+    auto const hash_u = std::hash<U>{}(pair.second);
+    return hash_t ^ hash_u;
+  }
+};
 } // namespace std
 
 void print(PI p, int i, unordered_map<PI, vector<pair<PI, int>>> &graph);
 
 auto solve() -> void {
-    auto dist = unordered_map<PI, ll>();
-    auto graph = unordered_map<PI, vector<pair<PI, int>>>();
-    int n, m, k, a, b, c, d, h;
-    priority_queue<PI> pq;
-    PI curr, curr_el;
-    int curr_w;
-    cin >> n >> m >> k;
-    vector<int> hx(n + 1);
-    LPI(i, 1, n + 1, 1) cin >> hx[i];
+  auto dist = unordered_map<PI, ll>();
+  auto graph = unordered_map<PI, vector<pair<PI, int>>>();
+  int n, m, k, a, b, c, d, h;
+  priority_queue<PI> pq;
+  PI curr, curr_el;
+  int curr_w;
+  cin >> n >> m >> k;
+  vector<int> hx(n + 1);
+  LPI(i, 1, n + 1, 1) cin >> hx[i];
 
-    LPI(i, 0, k, 1) {
-        cin >> a >> b >> c >> d >> h;
-        graph[{a, b}].PB({{c, d}, h});
+  LPI(i, 0, k, 1) {
+    cin >> a >> b >> c >> d >> h;
+    graph[{a, b}].PB({{c, d}, h});
+  }
+  // get edges on first floor
+  LPI(i, 2, m + 1, 1) {
+    auto current = graph[{1, i}];
+    if (!current.empty()) {
+      graph[{1, 1}].PB({{1, i}, abs(1 - i) * hx[1]});
     }
-    pq.push({1, 1});
-    while (!pq.empty()) {
-        curr = pq.top();
-        pq.pop();
-        if (graph[curr].empty()) {
-            LPI(i, 1, m + 1, 1) {
-                auto current = graph[{curr.first, i}];
-                if (!current.empty() || (curr.first == n && i == m)) {
-                    graph[curr].PB(
-                            {{curr.first, i}, abs(curr.second - i) * hx[curr.first]});
-                }
-            }
-        }
-        LPI(i, 0, graph[curr].size(), 1) {
-            // print(curr, i, graph);
-            curr_el = graph[curr][i].first;
-            curr_w = graph[curr][i].second;
-            if (!dist[curr_el] || dist[curr] + curr_w < dist[curr_el]) {
-                if (curr_el.first == curr.first) {
-                    dist[curr_el] = dist[curr] + curr_w;
-                } else {
-                    dist[curr_el] = dist[curr] - curr_w;
-                }
+  }
 
-                pq.push(curr_el);
-            }
+  pq.push({1, 1});
+  while (!pq.empty()) {
+    curr = pq.top();
+    pq.pop();
+    if (graph[curr].empty()) {
+      LPI(i, 1, m + 1, 1) {
+        auto current = graph[{curr.first, i}];
+        if (!current.empty() || (curr.first == n && i == m)) {
+          graph[curr].PB(
+              {{curr.first, i}, abs(curr.second - i) * hx[curr.first]});
         }
+      }
     }
+    LPI(i, 0, graph[curr].size(), 1) {
+      // print(curr, i, graph);
+      curr_el = graph[curr][i].first;
+      curr_w = graph[curr][i].second;
+      if (!dist[curr_el] || dist[curr] + curr_w < dist[curr_el]) {
+        if (curr_el.first == curr.first) {
+          dist[curr_el] = dist[curr] + curr_w;
+        } else {
+          dist[curr_el] = dist[curr] - curr_w;
+        }
 
-    dist[{n, m}] ? cout << dist[{n, m}] << '\n' : cout << "NO ESCAPE\n";
+        pq.push(curr_el);
+      }
+    }
+  }
+
+  dist[{n, m}] ? cout << dist[{n, m}] << '\n' : cout << "NO ESCAPE\n";
 }
 
 auto main() -> int {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
 
-    int _ = 1;
-    cin >> _;
-    while (_--)
-        solve();
+  int _ = 1;
+  cin >> _;
+  while (_--)
+    solve();
 
-    return 0;
+  return 0;
 }
 
 void print(PI p, int i, unordered_map<PI, vector<pair<PI, int>>> &graph) {
-    cout << "From: (" << p.first << ',' << p.second << ") to: (" << graph[p][i].first.first << ','
-         << graph[p][i].first.second
-         << ") with cost of: " << graph[p][i].second << '\n';
+  cout << "From: (" << p.first << ',' << p.second << ") to: ("
+       << graph[p][i].first.first << ',' << graph[p][i].first.second
+       << ") with cost of: " << graph[p][i].second << '\n';
 }
