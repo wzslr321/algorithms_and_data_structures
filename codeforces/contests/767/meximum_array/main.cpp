40ba129320b68b17ebb68d3df1b704ea0d273424
wzslr321 <wiktor.zajac888@gmail.com>
2022-01-23 13:44:27 +0100

    meximum array

--- codeforces/contests/767/meximum_array/main.cpp
+++ codeforces/contests/767/meximum_array/main.cpp
@@ -1,5 +1,3 @@
-#pragma GCC optimize("Ofast,unroll-loops")
-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -8,50 +6,36 @@ using namespace std;
 
 #define PB push_back
 
-#define LPI(i, a, b, d) for (int i = a; i < (int)b; i += d)
-#define CZYT(n, arr)                                                           \
-  for (int i = 0; i < n; ++i)                                                  \
-  cin >> arr[i]
-
 using VI = vector<int>;
 using PI = pair<int, int>;
 
-const int N = 2 * 1e5 + 7;
-
-auto find_mex(vector<VI> &occurrences, VI &used, int &start) -> int {
-  int i = 0;
-  int k = 0;
-  while (occurrences[i].size() > 0) {
-    int best = N;
-    for (auto in : occurrences[i])
-      if (!used[in] && in >= start) best = min(best, in);
-    if (best == N) break;
-    k = max(k, best), ++i;
-  }
-  !k ? start = N : start = k + 1;
-
-  return i;
-}
-
 auto solve() -> void {
   int n;
-  vector<VI> occurrences(N);
-  VI used(N);
-  int start = 0;
-
   cin >> n;
-  LPI(i, 0, n, 1) {
-    int num;
-    cin >> num;
-    occurrences[num].PB(i);
+  VI arr(n + 1);
+  vector<VI> occ(n + 2);
+  for (int i = 1; i <= n; ++i) {
+    cin >> arr[i];
+    occ[arr[i]].PB(i);
   }
 
-  VI mex;
-  while (start < n)
-    mex.PB(find_mex(occurrences, used, start));
+  int l = 1;
+  VI mexv;
+  while (l <= n) {
+    int mex = 0;
+    int r = l;
 
-  cout << mex.size() << '\n';
-  for (auto el : mex)
+    for (; mex <= n + 1; ++mex) {
+      auto it = lower_bound(occ[mex].begin(), occ[mex].end(), l);
+      if (it == occ[mex].end()) break;
+      r = max(r, *it);
+    }
+    mexv.PB(mex);
+    l = r + 1;
+  }
+
+  cout << mexv.size() << '\n';
+  for (auto el : mexv)
     cout << el << ' ';
   cout << '\n';
 }
