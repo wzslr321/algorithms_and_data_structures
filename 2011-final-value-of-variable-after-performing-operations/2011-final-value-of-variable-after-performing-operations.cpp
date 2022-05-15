243d644cc8fa972e6f1246d237d0cb637136c55a
Wiktor Zając <wiktor.zajac888@gmail.com>
2022-05-15 11:05:32 +0200

    Time: 20 ms (10.95%), Space: 14.1 MB (58.32%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+   int finalValueAfterOperations(vector<string>& operations) {
+      int ans = 0;
+      for (size_t i = 0; i < operations.size(); ++i) {
+         if (operations[i][1] == '-')
+           --ans;
+         else
+           ++ans;
+      }
+      return ans;
+    }
+};
