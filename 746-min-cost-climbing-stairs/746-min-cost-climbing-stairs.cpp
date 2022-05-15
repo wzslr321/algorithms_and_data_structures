2fd620262299e7c6a0e9a2fb99a1b2aaa1882760
Wiktor Zając <wiktor.zajac888@gmail.com>
2022-05-15 18:03:37 +0200

    Time: 12 ms (31.92%), Space: 13.8 MB (29.11%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -6,7 +6,7 @@ public:
         vector<int> best(N + 1);
         best[0] = cost[0], best[1] = cost[1];
         for(size_t i = 0; i < N - 1; ++i) {
-           best[i + 2]  = min({best[i] + cost[i + 2],  best[i + 1] + cost[i + 2]});
+           best[i + 2]  = min(best[i] + cost[i + 2],  best[i + 1] + cost[i + 2]);
         }
         return best[N];
     }
