a037e9a94e3acc9f9eb9c9a169b2e582ace09b1f
Wiktor Zając <wiktor.zajac888@gmail.com>
2022-05-15 11:07:36 +0200

    Time: 3 ms (58.82%), Space: 6.5 MB (67.32%) - LeetHub

---
+++ 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numDifferentIntegers(string word) {
+        int count{0};
+        unordered_map<string,bool>m{};
+        for(auto i{0}; i < word.size(); i++){
+           if(isdigit(word[i])){
+               string current_num{word[i]};
+               while(isdigit(word[i+1])){
+                   current_num += word[i+1];
+                   ++i;
+               }
+               for(auto j{0}; j < current_num.size(); j++){
+                   if(current_num[j] == '0'){
+                       current_num.erase(0,1);
+                       --j;
+                   } else {
+                       break;
+                   }
+               }
+               if(m[current_num]){
+                   continue;
+               }
+               m[current_num] = true;
+               ++count;
+           } 
+        }
+        return count;
+    }
+};
