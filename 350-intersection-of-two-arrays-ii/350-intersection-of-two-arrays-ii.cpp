c5b8d2e358a0586425b2b4c98313725f559e64b4
Wiktor Zając <wiktor.zajac888@gmail.com>
2022-05-16 13:13:48 +0200

    Time: 10 ms (36.79%), Space: 10.6 MB (32.16%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        
         int MAX = 1007;
+        
         vector<int> seen(MAX);
-        fill(seen.begin(), seen.end(), 0);
         vector<int> added(MAX);
-        fill(added.begin(), added.end(), 0);
+        vector<int> ans{};
+        
         for(const auto el: nums1)    
             ++seen[el];
-        vector<int> ans{};
-        for(const auto el: nums2)  {
-           if(seen[el] > added[el]) {
-               ++added[el];
-               ans.push_back(el);
-           }
-        }
+        for(const auto el: nums2)  
+           if(seen[el] > added[el]) ++added[el], ans.push_back(el);
+        
         return ans;
     }
 };
