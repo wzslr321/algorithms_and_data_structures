d1894cfe0f3f7bb138267288aaf74e7d0a4c66f5
Wiktor Zając <wiktor.zajac888@gmail.com>
2022-05-17 10:56:09 +0200

    Time: 6 ms (21.85%), Space: 6.3 MB (26.30%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -4,10 +4,11 @@ public:
     bool isHappy(int n) {
         if(seen[n]) return false;
         seen[n] = true;
-        auto s = to_string(n);
         auto num = 0;
-        for(const auto el : s){
-            num += pow((el - 48),2);
+        while(n > 0) {
+            auto d = n % 10;
+            n /= 10;
+            num += d * d;
         }
         if(num == 1) return true;
         return isHappy(num);
